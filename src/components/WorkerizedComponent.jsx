21a593b7d64265fd031182f2a494ea8f1234ab0c
tsarapke <tsarapke@gmail.com>
2021-02-19 14:55:40 +0300

    no message

--- src/components/WorkerizedComponent.jsx
+++ src/components/WorkerizedComponent.jsx
@@ -7,6 +7,10 @@ export default function WorkerizedComponent() {
         worker.addEventListener('message', (data) => {
             console.log(data);
         })
+
+        worker.postMessage();
+
+        worker.terminate();
     }, []);
 
     return (
